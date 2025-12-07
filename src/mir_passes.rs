723ec9fd2fd51c20e701930e6b38ba1ad96c31da
AryaveerSR <me.aryaveer@gmail.com>
2025-12-07 18:50:42 +0530

    copy elimination pass

--- src/mir_passes.rs
+++ src/mir_passes.rs
@@ -1,12 +1,13 @@
 use crate::{
     mir::{MirFun, MirModule},
     mir_passes::{
-        cfg::Cfg, rename_blocks::RenameBlocks, unreachable_blocks::UnreachableBlocks,
-        variables::VarUseAnalyzer,
+        cfg::Cfg, copy_elimination::CopyElimination, rename_blocks::RenameBlocks,
+        unreachable_blocks::UnreachableBlocks, variables::VarUseAnalyzer,
     },
 };
 
 mod cfg;
+mod copy_elimination;
 mod rename_blocks;
 mod unreachable_blocks;
 mod variables;
@@ -19,6 +20,7 @@ pub fn run_passes(module: &mut MirModule) {
     for fun in &mut module.funs {
         UnreachableBlocks::run(fun);
         RenameBlocks::run(fun);
+        CopyElimination::run(fun);
 
         let _cfg = Cfg::from(&*fun);
         let _var_use_info = VarUseAnalyzer::run(fun);
