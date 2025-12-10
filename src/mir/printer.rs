a8026db703f30458a8666c9b76f13a98d1b69a54
AryaveerSR <me.aryaveer@gmail.com>
2025-12-10 19:58:30 +0530

    improve formatting for binary instr

--- src/mir/printer.rs
+++ src/mir/printer.rs
@@ -74,7 +74,7 @@ impl Display for InstrKind {
             Self::ConstNum { value } => write!(f, "const {value}"),
             Self::Copy { src } => write!(f, "copy {src}"),
             Self::Unary { op, arg } => write!(f, "{op} {arg}"),
-            Self::Binary { op, lhs, rhs } => write!(f, "{op} {lhs} {rhs}"),
+            Self::Binary { op, lhs, rhs } => write!(f, "{op} {lhs}, {rhs}"),
             Self::Call { name, args } => write!(f, "call {name}({})", args.iter().join(", ")),
         }
     }
