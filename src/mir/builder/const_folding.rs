661ceaa2641a9f1382a03d4c085a7f031494cecb
AryaveerSR <me.aryaveer@gmail.com>
2025-12-11 21:15:54 +0530

    fix lints

--- src/mir/builder/const_folding.rs
+++ src/mir/builder/const_folding.rs
@@ -6,8 +6,8 @@ use crate::{
 impl UnOp {
     pub fn fold(&self, value: Value) -> Value {
         match self {
-            UnOp::Negate => Value::Num(-value.as_num()),
-            UnOp::Not => Value::Bool(!value.as_bool()),
+            Self::Negate => Value::Num(-value.as_num()),
+            Self::Not => Value::Bool(!value.as_bool()),
         }
     }
 }
@@ -15,18 +15,18 @@ impl UnOp {
 impl BinOp {
     pub fn fold(&self, lhs: Value, rhs: Value) -> Value {
         match self {
-            BinOp::Add => Value::Num(lhs.as_num() + rhs.as_num()),
-            BinOp::Sub => Value::Num(lhs.as_num() - rhs.as_num()),
-            BinOp::Mul => Value::Num(lhs.as_num() * rhs.as_num()),
-            BinOp::Div => Value::Num(lhs.as_num() / rhs.as_num()),
-            BinOp::Eq => Value::Bool(lhs == rhs),
-            BinOp::NotEq => Value::Bool(lhs != rhs),
-            BinOp::Lesser => Value::Bool(lhs.as_num() < rhs.as_num()),
-            BinOp::LesserEq => Value::Bool(lhs.as_num() <= rhs.as_num()),
-            BinOp::Greater => Value::Bool(lhs.as_num() > rhs.as_num()),
-            BinOp::GreaterEq => Value::Bool(lhs.as_num() >= rhs.as_num()),
-            BinOp::And => Value::Bool(lhs.as_bool() && rhs.as_bool()),
-            BinOp::Or => Value::Bool(lhs.as_bool() || rhs.as_bool()),
+            Self::Add => Value::Num(lhs.as_num() + rhs.as_num()),
+            Self::Sub => Value::Num(lhs.as_num() - rhs.as_num()),
+            Self::Mul => Value::Num(lhs.as_num() * rhs.as_num()),
+            Self::Div => Value::Num(lhs.as_num() / rhs.as_num()),
+            Self::Eq => Value::Bool(lhs == rhs),
+            Self::NotEq => Value::Bool(lhs != rhs),
+            Self::Lesser => Value::Bool(lhs.as_num() < rhs.as_num()),
+            Self::LesserEq => Value::Bool(lhs.as_num() <= rhs.as_num()),
+            Self::Greater => Value::Bool(lhs.as_num() > rhs.as_num()),
+            Self::GreaterEq => Value::Bool(lhs.as_num() >= rhs.as_num()),
+            Self::And => Value::Bool(lhs.as_bool() && rhs.as_bool()),
+            Self::Or => Value::Bool(lhs.as_bool() || rhs.as_bool()),
         }
     }
 }
