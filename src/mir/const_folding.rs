6c75767a55618420b1b41a31a3f6d43af055bfef
AryaveerSR <me.aryaveer@gmail.com>
2025-12-13 19:22:49 +0530

    add constant folding for cases where only one value is constant

--- src/mir/const_folding.rs
+++ src/mir/const_folding.rs
@@ -13,22 +13,49 @@ impl InstrKind {
                 UnOp::Not => Operand::Bool(!arg.as_bool()),
             }),
 
-            Self::Binary { op, lhs, rhs } => (lhs.is_const() && rhs.is_const()).then(|| match op {
-                BinOp::Add => Operand::Num(lhs.as_num() + rhs.as_num()),
-                BinOp::Sub => Operand::Num(lhs.as_num() - rhs.as_num()),
-                BinOp::Mul => Operand::Num(lhs.as_num() * rhs.as_num()),
-                BinOp::Div => Operand::Num(lhs.as_num() / rhs.as_num()),
+            Self::Binary { op, lhs, rhs } => (lhs.is_const() && rhs.is_const())
+                .then(|| match op {
+                    BinOp::Add => Operand::Num(lhs.as_num() + rhs.as_num()),
+                    BinOp::Sub => Operand::Num(lhs.as_num() - rhs.as_num()),
+                    BinOp::Mul => Operand::Num(lhs.as_num() * rhs.as_num()),
+                    BinOp::Div => Operand::Num(lhs.as_num() / rhs.as_num()),
 
-                BinOp::And => Operand::Bool(lhs.as_bool() && rhs.as_bool()),
-                BinOp::Or => Operand::Bool(lhs.as_bool() || rhs.as_bool()),
+                    BinOp::And => Operand::Bool(lhs.as_bool() && rhs.as_bool()),
+                    BinOp::Or => Operand::Bool(lhs.as_bool() || rhs.as_bool()),
 
-                BinOp::Eq => Operand::Bool(lhs == rhs),
-                BinOp::NotEq => Operand::Bool(lhs != rhs),
-                BinOp::Lesser => Operand::Bool(lhs.as_num() < rhs.as_num()),
-                BinOp::LesserEq => Operand::Bool(lhs.as_num() <= rhs.as_num()),
-                BinOp::Greater => Operand::Bool(lhs.as_num() > rhs.as_num()),
-                BinOp::GreaterEq => Operand::Bool(lhs.as_num() >= rhs.as_num()),
-            }),
+                    BinOp::Eq => Operand::Bool(lhs == rhs),
+                    BinOp::NotEq => Operand::Bool(lhs != rhs),
+                    BinOp::Lesser => Operand::Bool(lhs.as_num() < rhs.as_num()),
+                    BinOp::LesserEq => Operand::Bool(lhs.as_num() <= rhs.as_num()),
+                    BinOp::Greater => Operand::Bool(lhs.as_num() > rhs.as_num()),
+                    BinOp::GreaterEq => Operand::Bool(lhs.as_num() >= rhs.as_num()),
+                })
+                .or_else(|| {
+                    (lhs.is_const() || rhs.is_const())
+                        .then(|| {
+                            let (constant, non_const) = if lhs.is_const() {
+                                (*lhs, *rhs)
+                            } else {
+                                (*rhs, *lhs)
+                            };
+
+                            match op {
+                                BinOp::Add if constant.as_num() == 0 => Some(non_const),
+                                BinOp::Sub if constant.as_num() == 0 => Some(non_const),
+                                BinOp::Mul if constant.as_num() == 0 => Some(Operand::Num(0)),
+                                BinOp::Mul if constant.as_num() == 1 => Some(non_const),
+                                BinOp::Div if constant.as_num() == 1 => Some(non_const),
+
+                                BinOp::And if constant.as_bool() => Some(non_const),
+                                BinOp::And if !constant.as_bool() => Some(Operand::Bool(false)),
+                                BinOp::Or if constant.as_bool() => Some(Operand::Bool(true)),
+                                BinOp::Or if !constant.as_bool() => Some(non_const),
+
+                                _ => None,
+                            }
+                        })
+                        .flatten()
+                }),
         }
     }
 }
